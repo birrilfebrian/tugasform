20902cac6f4d9468b6d8bd7852d3fdedcfd68e2a
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2019-10-17 09:06:45 +0700

    Add files via upload

---
+++ aturanpenulisanvariablephp/MemberiNilaiPadaVariabel.php
@@ -0,0 +1,5 @@
+<?php
+ $nama="birril";//ini adalah inisiasi 
+ $umur="17";
+ $pesan="pengen jadi fullstack";
+?>
---
+++ aturanpenulisanvariablephp/VariabelPhpTidakBertipe.php
@@ -0,0 +1,5 @@
+<?php
+ $a="17";//otomatis menjadi tipedata (integer)
+ $a="birril";//otomatis menjadi tipe data (String)
+ $a="1.2"//otomatis menjadi tipe data (float)
+?>
